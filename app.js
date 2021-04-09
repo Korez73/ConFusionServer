0ee866e014c4c43be36b094d38cfef724e0a8374
Jamie Weldon <jamie.weldon@coyote.com>
2021-04-09 06:56:14 -0500

    cookies

--- app.js
+++ app.js
@@ -17,43 +17,55 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-  console.log('Connected correctly to server');
+    console.log('Connected correctly to server');
 }, (err) => { console.log(err); })
 
 
-
+//authorization function
 function auth(req, res, next) {
-  console.log('req.headers -------------------------------------');
-  console.log(req.headers);
 
-  var authHeader = req.headers.authorization;
-  console.log('authHeader -------------------------------------');
-  console.log(authHeader);
+    console.log("Cookie check!");
+    console.log(req.signedCookies);
+
+    if (!req.signedCookies.user) {
+
+        //console.log(req.headers);
+        var authHeader = req.headers.authorization;
+        // console.log(authHeader);        
+        
+        if(!authHeader) {
+            var err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+
+        console.log("I guess authHeader is a thing ------------------------------ ");
   
-  if(!authHeader) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
+        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    
+        var username = auth[0];
+        var password = auth[1];
 
-  console.log("I guess authHeader is a thing ------------------------------ ");
-  
-  //var auth = new Buffer(authHeader.split(' ')[1], 'base64'); //depreciated
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        if(username === 'admin' && password === 'password') {
+            res.cookie('user', 'admin', { signed:true });
+            next();
+        } else {
+            var err = new Error('Incorrect username and/or password');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+        }
 
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username === 'admin' && password === 'password') {
-    next();
-  } else {
-    var err = new Error('Incorrect username and/or password');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-  }
-
+    } else {
+        if (req.signedCookies.user === 'admin') {
+            next();
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
 }
 
 var app = express();
@@ -66,7 +78,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890'));
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -78,18 +90,18 @@ app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
